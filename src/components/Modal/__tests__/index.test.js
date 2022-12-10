a8b5bb3718c98544dde99b66702083f25df11698
MrPhuzzles <tieten.james92@gmail.com>
2022-12-09 19:25:10 -0500

    Added base test for Modal

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Modal from '../';
+
+const mockToggleModal = jest.fn();
+const currentPhoto = {
+    name: 'Park bench',
+    category: 'landscape',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    index: 1
+};
+
+afterEach(cleanup)
+
+describe('Modal component', () => {
+    it('renders', () => {
+        <Modal></Modal>
+    })
+});
